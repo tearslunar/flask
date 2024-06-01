dd4c68ae51e44a59eb414f8d0db623a9e9c2a922
tears <tearscg@gmail.com>
2024-06-01 23:37:56 +0900

    601

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route("/")
+def start():
+    return render_template("start.html")
+
+@app.route("/hello")
 def hello():
     return render_template("index.html")
  
